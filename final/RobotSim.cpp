affb71db885b7d4e4b924bf79ed89d2dd0f362d0
ndngo <ngonathanieldean@gmail.com>
2017-04-09 12:38:42 -0400

    Fixed parsing of telemetry packets

--- final/RobotSim.cpp
+++ final/RobotSim.cpp
@@ -14,8 +14,7 @@ void command(std::string ip, int port) {
 	char * ptr;
 	int time = 0;
 	int count = 0; 
-	
-	// Create MySocket obj
+	bool valid = false;	// Create MySocket obj
 	MySocket CommandSocket(SocketType::CLIENT, ip, port, ConnectionType::TCP, 100);
 	
 	// 3 way handshake
@@ -24,104 +23,113 @@ void command(std::string ip, int port) {
 		enum choice {ADVANCE = 1, RETREAT, SINISTER, DEXTER, RAISE, LOWER, RELEASE, HOLD, ZZZ};
 		// packet create
 		std::cout << "options:\n1-forward\n2-backward\n3-left\n4-right\n5-up\n6-down\n7-open\n8-close\n9-sleep" << std::endl;
-		
+		command = 0;
 		std::cout << "Enter command: ";
 		std::cin >> command;
 		std::cout << "Enter duration: ";
 		std::cin >> time;
 		MotorBody DriveCmd;
 		PktDef txPacket;
-
+		valid = false;
 		if (command == ADVANCE) { // "forward"
 			txPacket.SetCmd(DRIVE);
 			DriveCmd.Direction = FORWARD;
 			DriveCmd.Duration = time;
 			txPacket.SetBodyData((char*)&DriveCmd, 2);
-		}
-		else if (command == RETREAT) { // "backward"
+			valid = true;
+		} else if (command == RETREAT) { // "backward"
 			txPacket.SetCmd(DRIVE);
 			DriveCmd.Direction = BACKWARD;
 			DriveCmd.Duration = time;
 			txPacket.SetBodyData((char*)&DriveCmd, 2);
-		}
-		else if (command == SINISTER)	{	// "left"
+			valid = true;
+		} else if (command == SINISTER)	{	// "left"
 			txPacket.SetCmd(DRIVE);
 			DriveCmd.Direction = LEFT;
 			DriveCmd.Duration = time;
 			txPacket.SetBodyData((char*)&DriveCmd, 2);
-		}
-		else if (command == DEXTER) { // "right"
+			valid = true;
+		} else if (command == DEXTER) { // "right"
 			txPacket.SetCmd(DRIVE);
 			DriveCmd.Direction = RIGHT;
 			DriveCmd.Duration = time;
 			txPacket.SetBodyData((char*)&DriveCmd, 2);
-		}
-		else if (command == RAISE) { // up
+			valid = true;
+		} else if (command == RAISE) { // up
 			txPacket.SetCmd(ARM);
 			DriveCmd.Direction = UP;
 			DriveCmd.Duration = time;
 			txPacket.SetBodyData((char*)&DriveCmd, 2);
-		}
-		else if (command == LOWER) { // down
+			valid = true;
+		} else if (command == LOWER) { // down
 			txPacket.SetCmd(ARM);
 			DriveCmd.Direction = DOWN;
 			DriveCmd.Duration = time;
 			txPacket.SetBodyData((char*)&DriveCmd, 2);
-		}
-		else if (command == RELEASE) { // open
+			valid = true;
+		} else if (command == RELEASE) { // open
 			txPacket.SetCmd(CLAW);
 			DriveCmd.Direction = OPEN;
 			DriveCmd.Duration = time;
 			txPacket.SetBodyData((char*)&DriveCmd, 2);
-		}
-		else if (command == HOLD) { // close
+			valid = true;
+		} else if (command == HOLD) { // close
 			txPacket.SetCmd(CLAW);
 			DriveCmd.Direction = CLOSE;
 			DriveCmd.Duration = time;
 			txPacket.SetBodyData((char*)&DriveCmd, 2);
-		}
-		else if (command == ZZZ) { // sleep
+			valid = true;
+		} else if (command == ZZZ) { // sleep
 			txPacket.SetCmd(SLEEP);
+			valid = true;
 
 			// do not disconnect when sleep packet is sent
 			// wait for ACK(SLEEP) before cleanup
 			// ExeComplete = true;
 			// CommandSocket.DisconnectTCP();
+		} else {
+			// invalid command
+			std::cout << "Invalid command\n";
+			std::cin.clear();
+			std::cin.ignore(2000, '\n');
+			command = 0;
 		}
-		txPacket.SetPktCount(count++);
-		txPacket.CalcCRC();
-		ptr = txPacket.GenPacket();
+		if (command != 0) {
+			txPacket.SetPktCount(count++);
+			txPacket.CalcCRC();
+			ptr = txPacket.GenPacket();
 
-		CommandSocket.SendData(ptr, txPacket.GetLength());
-		
-		// receive data from robot
-		// crc check, check calculated crc, with given crc, calculate crc with packet, given crc in buffer
-		receiveData = new char[256];
-		int dataSize = CommandSocket.GetData(receiveData);
-		PktDef rxPacket(receiveData);
-		if (rxPacket.GetAck()) { std::cout << "ACK received" << std::endl; }
-		// process packet only if CRC is good
-		// wait for ack
-		// while bad ACK or is a NACK, resend cmd packet
-		// check if packet is well formed before checking if ACK
-		// while recv packet from robot is !ACK, then resend txPacket, until at what point should give up?
-		// for 7 times
-		// while (!rxPacket.CheckCRC(receiveData, dataSize) || !rxPacket.GetAck()) {
-		// while packet is well formed (check CRC), and packet is not an acknowledgement of the send cmd packet,
-		// and having only tried resending less than 7 times
-		// keep resending
-		for (unsigned i = 0; i < 7 && (!rxPacket.CheckCRC(receiveData, dataSize) || !rxPacket.GetAck()); i++) {
 			CommandSocket.SendData(ptr, txPacket.GetLength());
-			int dataSize = CommandSocket.GetData(receiveData);
-			rxPacket.CheckCRC(receiveData, dataSize);
-			rxPacket.SetBodyData(receiveData,dataSize);
-		}
-		// the cmd packet is well formed and robot has replied with ACK(cmd)
-		// if ACK(SLEEP) was received, begin cleanup process
 
-		if (rxPacket.GetCmd() == SLEEP) {
-			CommandSocket.DisconnectTCP();
-			ExeComplete = true;
+			// receive data from robot
+			// crc check, check calculated crc, with given crc, calculate crc with packet, given crc in buffer
+			receiveData = new char[256];
+			int dataSize = CommandSocket.GetData(receiveData);
+			PktDef rxPacket(receiveData);
+			if (rxPacket.GetAck()) { std::cout << "ACK received" << std::endl; }
+			// process packet only if CRC is good
+			// wait for ack
+			// while bad ACK or is a NACK, resend cmd packet
+			// check if packet is well formed before checking if ACK
+			// while recv packet from robot is !ACK, then resend txPacket, until at what point should give up?
+			// for 7 times
+			// while (!rxPacket.CheckCRC(receiveData, dataSize) || !rxPacket.GetAck()) {
+			// while packet is well formed (check CRC), and packet is not an acknowledgement of the send cmd packet,
+			// and having only tried resending less than 7 times
+			// keep resending
+			for (unsigned i = 0; i < 7 && (!rxPacket.CheckCRC(receiveData, dataSize) || !rxPacket.GetAck()); i++) {
+				CommandSocket.SendData(ptr, txPacket.GetLength());
+				int dataSize = CommandSocket.GetData(receiveData);
+				rxPacket.CheckCRC(receiveData, dataSize);
+				rxPacket.SetBodyData(receiveData, dataSize);
+			}
+			// the cmd packet is well formed and robot has replied with ACK(cmd)
+			// if ACK(SLEEP) was received, begin cleanup process
+
+			if (rxPacket.GetCmd() == SLEEP) {
+				CommandSocket.DisconnectTCP();
+				ExeComplete = true;
+			}
 		}
 	}
 }
@@ -147,38 +155,38 @@ void telemetry(std::string ip, int port) {
 
 			// psilay RAW
 			char * p = recvData;
-			std::cout << "display RAW: ";
+			std::cout << "RECEIVING DATA PACKET" << std::endl << "RAW Data Bytes: ";
 			for (int i = 0; i < size; i++) {
-				std::cout << std::hex << (unsigned int)*(p++);
+				std::cout << std::hex << (unsigned int)*(p++) << ",";
 			}
 
 			// display sonar reading, arm reading body
 			char * q = rxPacket.GetBodyData();
-			int bodysize = rxPacket.GetLength() - HEADERSIZE - sizeof(char);
-			std::cout << std::dec << "\nsonar value: " << (unsigned int) * (q++) << std::endl;
-			p++;
-			std::cout << std::dec << "\narm pos: " << (unsigned int) * (q++);
-			for (int i = 0; i < bodysize; i++) {
-				std::cout << std::dec << (unsigned int)*(q++) << " ";
-			}
-			// display drive
-			std::cout << "\nDRIVE: ";
-			if (rxPacket.GetCmd() == DRIVE) { std::cout << "1 "; }
-			else { std::cout << "0 "; }
 
-			// dispaly arm claw status
-			/*char * r = rxPacket.GetBodyData();
-			if ((*q >> 3) & 0x01) { std::cout << "arm is up "; }
-			else { std::cout << "arm is down ";  }
+ 			int bodysize = rxPacket.GetLength() - HEADERSIZE - sizeof(char);
+			std::cout << std::dec << "\nSonar Value: " << (unsigned int) *q << std::endl;
+			q += sizeof(short);
+
+			// arms
+			short armpos = *(short *)q;
+ 			std::cout << std::dec << "Arm Position: " << armpos;
+			q += sizeof(short);
+
+			// bits
 			
-			if ((*q >> 4) & 0x01) { std::cout << "claw is open"; }
-			else { std::cout << "claw is closed"; }
-			std::cout << std::endl;
-			*/
-			std::cout << "Packet count: " << std::dec << rxPacket.GetPktCount() << std::endl << std::endl;
+			std::cout << "\nStatus Bits:\nDrive: " << (int)(*q & 0x01);
+
+			std::cout << "\nArm is: ";
+			if ((*q >> 1) & 0x01) { std::cout << "UP"; }
+			if ((*q >> 2) & 0x01) { std::cout << "DOWN"; }
+			std::cout << "\nClaw is: ";
+			if ((*q >> 3) & 0x01) { std::cout << "OPEN"; }
+			if ((*q >> 4) & 0x01) { std::cout << "CLOSED"; }
+		
+			*q = (*q & 0x1F);
+
+			std::cout << "\nPacket count: " << std::dec << rxPacket.GetPktCount() << std::endl;
 		}
-
-
 	}
 }
 
